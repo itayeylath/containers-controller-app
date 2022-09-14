00fa45fade4d2f725fdbe5a53f59df32f937ed23
itayeylath <itay.ey@gmail.com>
2022-09-14 11:24:30 +0300

    deploy wiith           dima

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,74 @@
-const gulp = require('gulp');
-const clean = require('gulp-clean');
-const { exec } = require('child_process');
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const { exec } = require("child_process");
 
 // Removes previous dist
-gulp.task('clean', () => {
-  return gulp.src('./dist', { allowEmpty: true })
-    .pipe(clean());
+gulp.task("clean", () => {
+  return gulp.src("./dist", { allowEmpty: true }).pipe(clean());
 });
 
 // Initial ts compile
-gulp.task('tsc', cb => {
-  exec('tsc', () => cb());
+gulp.task("tsc", (cb) => {
+  exec("tsc", () => cb());
 });
 
 // Watch ts files and recompile
-gulp.task('tsc-w', () => {
-  const tsc = exec('tsc -w --preserveWatchOutput --pretty');
+gulp.task("tsc-w", () => {
+  const tsc = exec("tsc -w --preserveWatchOutput --pretty");
 
-  tsc.stdout.on('data', data => console.log(data));
-  tsc.stderr.on('data', data => console.error(data));
+  tsc.stdout.on("data", (data) => console.log(data));
+  tsc.stderr.on("data", (data) => console.error(data));
 
-  tsc.on('close', code => console.log(`tsc exited with code ${code}`));
+  tsc.on("close", (code) => console.log(`tsc exited with code ${code}`));
 });
 
 // Start express
-gulp.task('express', () => {
-  const tsc = exec('nodemon --watch ./src/server ./src/server/server.ts');
-  tsc.stdout.on('data', data => console.log(data));
-  tsc.stderr.on('data', data => console.error(data));
+gulp.task("express", () => {
+  const tsc = exec("nodemon --watch ./src/server ./src/server/server.ts");
+  tsc.stdout.on("data", (data) => console.log(data));
+  tsc.stderr.on("data", (data) => console.error(data));
 });
 
 // Build all
-gulp.task('build', gulp.series(
-  'clean',
-  'tsc',
-));
+gulp.task("build", gulp.series("clean", "tsc"));
 
 // Heroku copy client files
-gulp.task('heroku-copy-client', () => {
-  return gulp.src([
-    './src/client/build/**/*',
-  ])
-    .pipe(gulp.dest('./deploy/client'));
+gulp.task("heroku-copy-client", () => {
+  return gulp
+    .src(["./src/client/build/**/*"])
+    .pipe(gulp.dest("./deploy/client"));
 });
 
 // Heroku copy non-client files
-gulp.task('heroku-copy-non-client', () => {
-  return gulp.src([
-    './package.json',
-    './package-lock.json',
-    './Procfile',
-    './dist/server/server.js',
-  ])
-    .pipe(gulp.dest('./deploy'));
+gulp.task("heroku-copy-non-client", () => {
+  return gulp
+    .src([
+      "./package.json",
+      "./package-lock.json",
+      "./Procfile",
+      "./dist/server/server.js",
+    ])
+    .pipe(gulp.dest("./deploy"));
 });
 
 // Heroku clean files
-gulp.task('heroku-clean', () => {
-  return gulp.src([
-    './deploy',
-  ], { allowEmpty: true })
-    .pipe(clean());
+gulp.task("heroku-clean", () => {
+  return gulp.src("./deploy", { allowEmpty: true }).pipe(clean());
 });
 
 // Heroku deploy
-gulp.task('deploy', gulp.series(
-  'heroku-clean',
-  'build',
-  'heroku-copy-client',
-  'heroku-copy-non-client',
-));
+gulp.task(
+  "deploy",
+  gulp.series(
+    "heroku-clean",
+    "build",
+    "heroku-copy-client",
+    "heroku-copy-non-client"
+  )
+);
 
 // Run all (without express)
-gulp.task('dev', gulp.series(
-  'build',
-  gulp.parallel(
-    'tsc-w',
-  ),
-));
+gulp.task("dev", gulp.series("build", gulp.parallel("tsc-w")));
 
 // Run all together
-gulp.task('default', gulp.series(
-  'build',
-  gulp.parallel(
-    'tsc-w',
-    'express',
-  ),
-));
+gulp.task("default", gulp.series("build", gulp.parallel("tsc-w", "express")));
--- src/client/src/helpers/containers-server-req.ts
+++ src/client/src/helpers/containers-server-req.ts
@@ -4,26 +4,26 @@ import { Container } from "../types/containers-table-types";
 //All requests from the server to CRUD (create,read,update,delete)- containers.
 export const reqAddContainer = async (data: Container) => {
   await axios.post(
-    "http://localhost:4040/containers/add",
+    "https://containers-controller-app.herokuapp.com/containers/add",
     data
   );
 };
 export const reqGetContainers = async () => {
   const values = await axios.get(
-    "http://localhost:4040/containers/data/all"
+    "https://containers-controller-app.herokuapp.com/containers/data/all"
   );
   return values.data;
 };
 // TODO: add get by id
 export const reqUpdateContainer = async (data: Container) => {
   await axios.put(
-    "http://localhost:4040/containers/update",
+    "https://containers-controller-app.herokuapp.com/containers/update",
     data
   );
 };
 export const reqDeleteContainer = async (id: number) => {
   await axios.delete(
-    "http://localhost:4040/containers/delete/" + id
+    "https://containers-controller-app.herokuapp.com/containers/delete/" + id
   );
 };
 
